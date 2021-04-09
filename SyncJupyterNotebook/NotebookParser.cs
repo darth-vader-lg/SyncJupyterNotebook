6d3aec15ee2be15c31ae75556c19dcc73c486501
darth-vader-lg <luigi.generale@gmail.com>
2021-04-10 00:01:20 +0200

    Aggiunto il parser di argomenti della linea di comando ed il parser di files notebook.

---
+++ SyncJupyterNotebook/NotebookParser.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Text;
+
+namespace SyncJupyterNotebook
+{
+   /// <summary>
+   /// Parser di file ipynb
+   /// </summary>
+   partial class NotebookParser
+   {
+      #region Fields
+      /// <summary>
+      /// Ultimo carattere letto
+      /// </summary>
+      private char c;
+      /// <summary>
+      /// Colonna attuale
+      /// </summary>
+      private int col;
+#if DEBUG
+      /// <summary>
+      /// Posizione corrente di parsing
+      /// </summary>
+      string currentLinePosition;
+#endif
+      /// <summary>
+      /// File ipynb
+      /// </summary>
+      private readonly string ipynb;
+      /// <summary>
+      /// Indice attuale nel testo
+      /// </summary>
+      private int ix;
+      /// <summary>
+      /// Riga attuale
+      /// </summary>
+      private int row;
+      #endregion
+      #region Delegates
+      /// <summary>
+      /// Delegato di parserizzazione del valore di un elemento
+      /// </summary>
+      /// <param name="item">Item corrente</param>
+      /// <returns>true se la parserizzazione e' avvenuta correttamente</returns>
+      private delegate bool ParseItemValue(Item item);
+      /// <summary>
+      /// Delegato di parserizzazione del contenuto di una sezione
+      /// </summary>
+      /// <param name="item">Item corrente</param>
+      /// <returns>true se la parserizzazione e' avvenuta correttamente</returns>
+      private delegate bool ParseSectionContent(Item item);
+      #endregion
+      #region Methods
+      /// <summary>
+      /// Costruttore
+      /// </summary>
+      /// <param name="text">Testo</param>
+      private NotebookParser(string text)
+      {
+         ipynb = text;
+#if DEBUG
+         currentLinePosition = ipynb;
+#endif
+      }
+      /// <summary>
+      /// Legge un carattere e lo memorizza in c
+      /// </summary>
+      /// <returns>Il carattere letto</returns>
+      private char GetChar()
+      {
+         if (ix >= ipynb.Length)
+            return c = '\0';
+         c = ipynb[ix++];
+         if (c == '\n') {
+            row++;
+            col = 0;
+         }
+#if DEBUG
+         currentLinePosition = ipynb[ix..];
+#endif
+         return c;
+      }
+      /// <summary>
+      /// Parserizza un testo
+      /// </summary>
+      /// <param name="text"></param>
+      /// <returns></returns>
+      internal static Item Parse(string text)
+      {
+         var parser = new NotebookParser(text);
+         var root = new Item("Root");
+         parser.ParseValue(root);
+         return root;
+      }
+      /// <summary>
+      /// Parserizza un elemento
+      /// </summary>
+      /// <param name="item">Item corrente</param>
+      /// <param name="parseItemValue">Funzione di parsing del valore di un elemento</param>
+      /// <returns></returns>
+      private bool ParseItem(Item item, ParseItemValue parseItemValue)
+      {
+         SkipSeparators();
+         if (c != '\"')
+            return false;
+         GetChar();
+         var name = new StringBuilder();
+         while (GetChar() != '\"')
+            name.Append(c);
+         SkipSeparators();
+         if (GetChar() != ':')
+            throw new Exception($"Invalid key declaration at row {row}, col {col}");
+         var childItem = new Item(name.ToString().Trim());
+         if (!parseItemValue(childItem))
+            return false;
+         item.Add(childItem);
+         return true;
+      }
+      /// <summary>
+      /// Parserizza una sezione
+      /// </summary>
+      /// <param name="item">Item corrente</param>
+      /// <param name="brackets">Tipi di parentesi</param>
+      /// <param name="parseSectionContent">Funzione di parsing del contenuto della sezione</param>
+      /// <returns>true se parsing avvenuto correttamente</returns>
+      private bool ParseSection(Item item, string brackets, ParseSectionContent parseSectionContent)
+      {
+         if (brackets.Length > 1)
+            while (GetChar() != brackets[0] && c != '\0') ;
+         if (brackets[0] != '\"')
+            SkipSeparators();
+         if (!parseSectionContent(item))
+            return false;
+         SkipSeparators();
+         return GetChar() == brackets[^1];
+      }
+      /// <summary>
+      /// Parserizza un valore
+      /// </summary>
+      /// <param name="item">Item corrente</param>
+      /// <returns>true se parsing avvenuto correttamente</returns>
+      private bool ParseValue(Item item)
+      {
+         SkipSeparators();
+         switch (c) {
+            case '{':
+               if (!ParseSection(item, "{}", sectionItem =>
+               {
+                  while (ParseItem(sectionItem, valueItem => ParseValue(valueItem))) ;
+                  return true;
+               }))
+                  return false;
+               break;
+            case '[':
+               if (!ParseSection(item, "[]", sectionItem =>
+               {
+                  var arrayItem = new Item(null);
+                  while (ParseValue(arrayItem) && c == ',') {
+                     sectionItem.Add(arrayItem);
+                     arrayItem = new Item(null);
+                  }
+                  return true;
+               }))
+                  return false;
+               break;
+            case '\"':
+               if (!ParseSection(item, "\"\"", valueItem =>
+               {
+                  var cPrev = '\0';
+                  var value = new StringBuilder();
+                  while (ipynb[ix] != '\"' || cPrev == '\\')
+                     value.Append(cPrev = GetChar());
+                  item.Value = value.ToString();
+                  return true;
+               }))
+                  return false;
+               break;
+            default:
+               var value = new StringBuilder();
+               while (ix < ipynb.Length && (c = ipynb[ix]) != ',' && c != '}' && c != ']')
+                  value.Append(GetChar());
+               item.Value = value.ToString().Trim();
+               break;
+         }
+         SkipSeparators();
+         if (c == ',')
+            GetChar();
+         return ix < ipynb.Length;
+      }
+      /// <summary>
+      /// Salta i separatori
+      /// </summary>
+      private void SkipSeparators()
+      {
+         if (ix >= ipynb.Length)
+            return;
+         while ((c = ipynb[ix]) == ' ' || c == '\t' || c == '\r' || c == '\n') {
+            if (GetChar() == '\0')
+               break;
+         }
+      }
+      #endregion
+   }
+
+   partial class NotebookParser // Item
+   {
+      /// <summary>
+      /// Item del file
+      /// </summary>
+      [DebuggerDisplay("{DebuggerDisplay,nq}")]
+      internal class Item : KeyedCollection<string, Item>
+      {
+         #region Properties
+         /// <summary>
+         /// Funzione di visualizzazione di debug
+         /// </summary>
+         private string DebuggerDisplay => $"{Name}{(Count > 0 ? $", [{Count}]" : "")}{(Value != null ? $", {Value}" : "")}";
+         /// <summary>
+         /// Nome dell'item
+         /// </summary>
+         public string Name { get; private set; }
+         /// <summary>
+         /// Valore
+         /// </summary>
+         public string Value { get; set; }
+         #endregion
+         #region Methods
+         /// <summary>
+         /// Costruttore
+         /// </summary>
+         /// <param name="name">Nome dell'item</param>
+         /// <param name="value">Eventuale valore</param>
+         public Item(string name, string value = null)
+         {
+            Name = name;
+            Value = value;
+         }
+         /// <summary>
+         /// Restituisce la chiave per l'item
+         /// </summary>
+         /// <param name="item">L'item</param>
+         /// <returns>La chiave</returns>
+         protected override string GetKeyForItem(Item item)
+         {
+            if (!string.IsNullOrEmpty(item.Name))
+               return item.Name;
+            for (var i = 0; i < Count; i++) {
+               if (this[i] == item)
+                  return $"[{i}]";
+            }
+            var newKey = $"[{Count}]";
+            return newKey;
+         }
+         #endregion
+      }
+   }
+}
--- SyncJupyterNotebook/Program.cs
+++ SyncJupyterNotebook/Program.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
-namespace UpdateJupyterNotebook
+namespace SyncJupyterNotebook
 {
    /// <summary>
    /// Programma
    /// </summary>
-   class Program
+   partial class Program
    {
       #region Methods
       /// <summary>
@@ -14,7 +19,229 @@ namespace UpdateJupyterNotebook
       /// <param name="args">Argomenti</param>
       static void Main(string[] args)
       {
+         Parser.Default.ParseArguments<OptionsSyncNotebook, OptionsSyncPython>(args)
+            .MapResult(
+            (OptionsSyncNotebook opt) => RunSyncNotebook(opt),
+            (OptionsSyncPython opt) => RunSyncPython(opt),
+            errors => 1);
+      }
+      /// <summary>
+      /// Funzione di sincronizzazione del notebook
+      /// </summary>
+      /// <param name="opt">Opzioni</param>
+      static private int RunSyncNotebook(OptionsSyncNotebook opt)
+      {
+         if (!File.Exists(opt.Path)) {
+            Console.Error.WriteLine($"Error: the notebook {opt.Path} doesn't exist!");
+            return 1;
+         }
+//         var ipynb = File.ReadAllText(opt.Path);
+//         var result = new StringBuilder();
+//         var ix = 0;
+//         var c = '\0';
+//         var col = 0;
+//         var row = 0;
+//#if DEBUG
+//         var currentLinePosition = ipynb;
+//#endif
+//         // Legge un carattere e lo memorizza in c
+//         char GetChar(bool skip)
+//         {
+//            if (ix >= ipynb.Length)
+//               return c = '\0';
+//            c = ipynb[ix++];
+//            if (!skip)
+//               result.Append(c);
+//            if (c == '\n') {
+//               row++;
+//               col = 0;
+//            }
+//#if DEBUG
+//            currentLinePosition = ipynb[ix..];
+//#endif
+//            return c;
+//         }
+//         void SkipSeparators(bool skip)
+//         {
+//            if (ix >= ipynb.Length)
+//               return;
+//            while ((c = ipynb[ix]) == ' ' || c == '\t' || c == '\r' || c == '\n') {
+//               if (GetChar(skip) == '\0')
+//                  break;
+//            }
+//         }
+//         // Apre una parte fra delimitatori
+//         bool OpenDelims(bool skip, string brackets, Func<bool> execute)
+//         {
+//            if (brackets.Length > 1)
+//               while (GetChar(skip) != brackets[0] && c != '\0') ;
+//            SkipSeparators(skip);
+//            if (!execute())
+//               return false;
+//            SkipSeparators(skip);
+//            return GetChar(skip) == brackets[^1];
+//         }
+//         // Cerca una chiave
+//         bool GetKey(bool skip, Func<string, bool> execute)
+//         {
+//            SkipSeparators(skip);
+//            if (c != '\"')
+//               return false;
+//            GetChar(skip);
+//            var name = new StringBuilder();
+//            while (GetChar(skip) != '\"')
+//               name.Append(c);
+//            SkipSeparators(skip);
+//            if (GetChar(skip) != ':')
+//               throw new Exception($"Invalid key declaration at row {row}, col {col}");
+//            if (!execute(name.ToString()))
+//               return false;
+//            return true;
+//         }
+//         bool GetValue(bool skip)
+//         {
+//            SkipSeparators(skip);
+//            switch (c) {
+//               case '{':
+//                  if (!OpenDelims(skip, "{}", () =>
+//                  {
+//                     while (GetKey(skip, k => GetValue(skip))) ;
+//                     return true;
+//                  }))
+//                     return false;
+//                  break;
+//               case '[':
+//                  if (!OpenDelims(skip, "[]", () =>
+//                  {
+//                     while (GetValue(skip) && c == ',') ;
+//                     return true;
+//                  }))
+//                     return false;
+//                  break;
+//               case '\"':
+//                  if (!OpenDelims(skip, "\"\"", () =>
+//                  {
+//                     var cPrev = '\0';
+//                     while (ipynb[ix] != '\"' || cPrev == '\\')
+//                        cPrev = GetChar(skip);
+//                     return true;
+//                  }))
+//                     return false;
+//                  break;
+//               default:
+//                  while (ix < ipynb.Length && (c = ipynb[ix]) != ',' && c != '}' && c != ']')
+//                     GetChar(skip);
+//                  break;
+//            }
+//            SkipSeparators(skip);
+//            if (c == ',')
+//               GetChar(skip);
+//            return ix < ipynb.Length;
+//         }
+         //OpenDelims(false, "{}", () =>
+         //{
+         //   while (GetKey(false, key =>
+         //   {
+         //      if (key == "cells") {
+         //         while (OpenDelims(false, "[]", () =>
+         //         {
+         //            while (OpenDelims(false, "{}", () =>
+         //            {
+         //               while (GetKey(false, key =>
+         //               {
+         //                  if (key == "source")
+         //                     return GetValue(false);
+         //                  return GetValue(false);
+         //               })) ;
+         //               return true;
+         //            }));
+         //            return true;
+         //         }));
+         //         return true;
+         //      }
+         //      return GetValue(false);
+         //   }));
+         //   return true;
+         //});
+
+         var root = NotebookParser.Parse(File.ReadAllText(opt.Path));
+         foreach (var cell in root["cells"]) {
+            foreach (var line in cell["source"]) {
+               Console.WriteLine(line.Value);
+            }
+         }
+         return 0;
+      }
+      /// <summary>
+      /// Funzione di sincronizzazione dei files python
+      /// </summary>
+      /// <param name="opt">Opzioni</param>
+      static private int RunSyncPython(OptionsSyncPython opt)
+      {
+         if (!File.Exists(opt.Path)) {
+            Console.Error.WriteLine($"Error: the notebook {opt.Path} doesn't exist!");
+            return 1;
+         }
+         return 0;
       }
       #endregion
+   }
+
+   partial class Program // OptionsSyncNotebook
+   {
+      /// <summary>
+      /// Opzioni di sincronizzazione Python -> Notebook
+      /// </summary>
+      [Verb("nb", HelpText = "Syncronize a notebook with the related python files.")]
+      class OptionsSyncNotebook
+      {
+         #region Properties
+         /// <summary>
+         /// Path della directory radice dei repositories
+         /// </summary>
+         [Value(0, Default = ".", HelpText = "The path of notebook.")]
+         public string Path { get; set; }
+         /// <summary>
+         /// Esempi di utilizzo
+         /// </summary>
+         [Usage]
+         public static IEnumerable<Example> Examples
+         {
+            get => new List<Example>()
+            {
+               new("Syncronize a notebook with the related python files", new OptionsSyncNotebook { Path = @"path\to\the\notebook" }),
+            };
+         }
+         #endregion
+      }
+   }
+
+   partial class Program // OptionsSyncPython
+   {
+      /// <summary>
+      /// Opzioni di sincronizzazione Python -> Notebook
+      /// </summary>
+      [Verb("py", HelpText = "Syncronize the python files with the content of a notebook.")]
+      class OptionsSyncPython
+      {
+         #region Properties
+         /// <summary>
+         /// Path della directory radice dei repositories
+         /// </summary>
+         [Value(0, Default = ".", HelpText = "The path of notebook.")]
+         public string Path { get; set; }
+         /// <summary>
+         /// Esempi di utilizzo
+         /// </summary>
+         [Usage]
+         public static IEnumerable<Example> Examples
+         {
+            get => new List<Example>()
+            {
+               new("Syncronize the python files with the content of a notebook", new OptionsSyncNotebook { Path = @"path\to\the\notebook" }),
+            };
+         }
+         #endregion
+      }
    }
 }
