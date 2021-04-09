7b5beb4a2debec14c4252aca2bdf7f5e8ed1e6cf
darth-vader-lg <luigi.generale@gmail.com>
2021-04-09 09:55:46 +0200

    Aggiungere i file di progetto.

---
+++ SyncJupyterNotebook/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UpdateJupyterNotebook
+{
+   /// <summary>
+   /// Programma
+   /// </summary>
+   class Program
+   {
+      #region Methods
+      /// <summary>
+      /// Entry point
+      /// </summary>
+      /// <param name="args">Argomenti</param>
+      static void Main(string[] args)
+      {
+      }
+      #endregion
+   }
+}
